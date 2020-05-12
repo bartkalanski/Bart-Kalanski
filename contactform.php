c31c37c427fb9cc96b93d659527241303a9857b8
bkalan011 <61667381+bkalan011@users.noreply.github.com>
2020-05-12 19:03:46 +0100

    Changed receiving email in contact form

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "bkalanski@gmail.com";
+    $mailTo = "bkalanski@hotmail.co.uk";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
